81400f15fe8100d89f65d4b740d6f7753a5fdddc
mullachv <mullachv@gmail.com>
2025-06-04 21:04:57 -0400

    Fix typos in utility docs

--- torch/utils/_import_utils.py
+++ torch/utils/_import_utils.py
@@ -7,9 +7,9 @@ import torch
 
 
 def _check_module_exists(name: str) -> bool:
-    r"""Returns if a top-level module with :attr:`name` exists *without**
-    importing it. This is generally safer than try-catch block around a
-    `import X`. It avoids third party libraries breaking assumptions of some of
+    r"""Returns if a top-level module with :attr:`name` exists *without*
+    importing it. This is generally safer than a try/except block around an
+    ``import X``. It avoids third party libraries breaking assumptions of some of
     our tests, e.g., setting multiprocessing start method when imported
     (see librosa/#747, torchvision/#544).
     """
